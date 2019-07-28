e391571e8af2b7c9fe43e7c9e525db36983df293
Thilagamadhav <53383553+Thilagamadhav@users.noreply.github.com>
2019-07-28 10:28:00 -0500

    Update spring-boot-java8-micro_src_main_java_hello_GreetingController.java

--- spring-boot-java8-micro_src_main_java_hello_GreetingController.java
+++ spring-boot-java8-micro_src_main_java_hello_GreetingController.java
@@ -14,7 +14,7 @@ import java.util.Random;
 import java.util.function.Function;
 
 @Controller
-public class GreetingController {
+public class WelcomeController {
 
     private Log log = LogFactory.getLog(GreetingController.class);
 
